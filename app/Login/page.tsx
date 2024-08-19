570421039859f566f60dafb337ba522efa11e28e
PKNaveen <88645434+PKNaveen@users.noreply.github.com>
2024-08-19 08:53:40 +0530

    added shadcdn form

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,10 +1,43 @@
+import Image from "next/image";
+import PatientForm from "@/components/PatientForm";
+
+
 
 
 export default function Login() {
     return (
-        <div className='flex justify-center items-center min-h-screen '>
-            <p className='text-center'> Login!!</p>
-        </div>
+        // basic login page with login form
+        <div className="flex h-screen max-h-screen">
+       {/*     center the section vertically*/}
+       <section className="my-auto container">
+           {/*custom tailwind to remove scrollbar and add a max width of the logo*/}
+           <div className=" remove-scrollbar max-w=[500px]">
+               <Image
+                   src="/assets/icons/logo-full.svg"
+                   alt="logo"
+                   width={1000}
+                   height={1000}
+                    className="h-12 w-fit mb-12"
+               />
+           </div>
+           {/*Form component here*/}
+           <PatientForm/>
 
+           {/*Footer here*/}
+           <div className="text-14-regular">
+               <p className="justify-items-end text-dark-600 xl:text-left">
+                &copy; HealthApp
+               </p>
+           </div>
+       </section>
+            <Image src="/assets/images/onboarding-img.png"
+                   alt="patient"
+                   height={1000}
+                   width={1000}
+                   className="side-img max-w-[50%]"
+            />
+        </div>
     );
 }
+
+
